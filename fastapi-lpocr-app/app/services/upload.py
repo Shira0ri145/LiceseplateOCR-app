205fd780083de4d61badf24ffef373b9b9fdb43c
Athibdee <ath.145bd@gmail.com>
2024-11-02 19:25:45 +0700

    Update : add image upload prediction and commit to DB

--- fastapi-lpocr-app/app/services/upload.py
+++ fastapi-lpocr-app/app/services/upload.py
@@ -19,7 +19,7 @@ from app.schemas.upload import UploadFileCreate
 settings = get_settings()
 blob_service_client = BlobServiceClient.from_connection_string(settings.AZURE_CONNECTION_STRING)
 ALLOWED_IMAGE_EXTENSIONS = {"jpg", "jpeg", "png"}
-ALLOWED_VIDEO_MIME = "video/"
+ALLOWED_VIDEO_EXTENSION = {"mp4"}
 
 class UploadFileService:
     def __init__(self):
@@ -50,18 +50,23 @@ class UploadFileService:
                     detail="Invalid image format. Only jpg, jpeg, and png are allowed."
                 )
             upload_type = "image"
-            # obj_detect_url = await self._predict_image(file, container_client)
+            obj_detect_url = await self._predict_image(file, container_client)
             
-        elif file.content_type.startswith(ALLOWED_VIDEO_MIME):
+        elif file.content_type.startswith("video/"):
+            ext = file.filename.split(".")[-1].lower()
+            if ext not in ALLOWED_VIDEO_EXTENSION:
+                raise HTTPException(
+                    status_code=status.HTTP_400_BAD_REQUEST,
+                    detail="Invalid video format. Only mp4 is allowed."
+                )
             upload_type = "video"
-            upload_url = await self.upload_defaultfile(file, container_client)
-
             obj_detect_url = await self._predict_video(file, container_client)
         else:
             raise HTTPException(
                 status_code=status.HTTP_400_BAD_REQUEST,
-                detail="Invalid file type. Only images (jpg, jpeg, png) and videos are allowed."
+                detail="Invalid file type. Only images (jpg, jpeg, png) and videos (mp4) are allowed."
             )
+
 
 
         # checking all response
@@ -112,36 +117,53 @@ class UploadFileService:
             return None
     
     # service method
-    async def upload_defaultfile(self, file: UploadFile, container_client) -> str:
-    # Upload file to Azure Blob Storage
-        # defaultfile_blob_client = container_client.get_blob_client(file.filename)
-
-        # Read the file content and upload it
-        pass
-        
-        # upload_url = f"https://{settings.AZURE_ACCOUNT_NAME}.blob.core.windows.net/{settings.AZURE_CONTAINER_NAME}/{file.filename}"
-        # return upload_url
 
     async def _predict_image(self, file: UploadFile, container_client) -> str:
+        _, ext = os.path.splitext(file.filename)
+        ext = ext.lower() 
 
-            '''input_image = await file.read()'''
+        with tempfile.NamedTemporaryFile(delete=False, suffix=ext) as temp_image:
+            temp_image.write(await file.read())
+            temp_image_path = temp_image.name
+        
+        original_blob_name = file.filename
+        original_blob_client = container_client.get_blob_client(original_blob_name)
+        
+        with open(temp_image_path, "rb") as original_file:
+            video_data_original = io.BytesIO(original_file.read())
+            original_blob_client.upload_blob(video_data_original, overwrite=True, content_settings=ContentSettings(content_type='image/jpeg'))
 
-            # Object Detection
-            '''input_image = cv2.imdecode(np.frombuffer(input_image, np.uint8), cv2.IMREAD_COLOR)
-            results = self.model(input_image)'''
+        image = cv2.imread(temp_image_path)
+        assert image is not None, "Error reading image file"
+
+        results = self.model.predict(image, show=False)
+        boxes = results[0].boxes.xyxy.cuda().tolist()
+        clss = results[0].boxes.cls.cuda().tolist()
+        annotator = Annotator(image, line_width=2, example=self.names)
+
+        if boxes is not None:
+            for box, cls in zip(boxes, clss):
+                annotator.box_label(box, color=colors(int(cls), True), label=self.names[int(cls)])
+
+        output_path = os.path.join(tempfile.gettempdir(), "processed_image.jpg")
+        cv2.imwrite(output_path, image)
+
+
+        blob_name = f"predicted_{file.filename}"
+        blob_client = container_client.get_blob_client(blob_name)
+
+        with open(output_path, "rb") as output_file:
+            image_data_predicted = io.BytesIO(output_file.read())
+            blob_client.upload_blob(image_data_predicted, overwrite=True, content_settings=ContentSettings(content_type='image/jpeg'))
+
+        os.remove(temp_image_path)
+        os.remove(output_path)
+
+        image_url = f"https://{settings.AZURE_ACCOUNT_NAME}.blob.core.windows.net/{settings.AZURE_CONTAINER_NAME}/{blob_name}"
+        return image_url
+
             
-
-            return "https://www.example.com/"
-            
-            
-            '''cv2.imwrite(output_image_path, detected_image)
-
-            # Upload to Azure Blob Storage
-            detected_blob_client = container_client.get_blob_client(f"detected_{file.filename}")
-            with open(output_image_path, "rb") as detected_image_file:
-                detected_blob_client.upload_blob(detected_image_file, overwrite=True)
-
-            return f"https://{settings.AZURE_ACCOUNT_NAME}.blob.core.windows.net/{settings.AZURE_CONTAINER_NAME}/detected_{file.filename}"'''
+        
 
     async def _predict_video(self, file: UploadFile, container_client) -> str:
         
@@ -193,12 +215,6 @@ class UploadFileService:
         # ปิดการเปิดวิดีโอและบันทึกวิดีโอ
         cap.release()
         out.release()
-        '''
-        original_blob_name = file.filename
-        original_blob_client = container_client.get_blob_client(original_blob_name)
-        video_data_original =  io.BytesIO(await file.read())
-        original_blob_client.upload_blob(video_data_original, overwrite=True, content_settings=ContentSettings(content_type='video/mp4'))
-        '''
 
         # upload predicted to Azure Blob Storage
         blob_name = f"predicted_{file.filename}"
